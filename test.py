3f03fde0e7fbba4c77d2a6fc738bf5522bc14ed1
Andrey Radyhin <84941386+Andrii-Radyhin@users.noreply.github.com>
2022-08-17 05:49:16 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
-BASE_DIR = 'airbus-ship-detection'
-TEST_DIR = BASE_DIR + '/test_v2/'
+from config import *
+from utils import generators
+
+BASE_DIR = ''
+TEST_DIR = BASE_DIR + 'test_v2/'
 
 test_imgs = ['00dc34840.jpg', '00c3db267.jpg', '00aa79c47.jpg', '00a3a9d72.jpg']
 
@@ -9,14 +12,21 @@ from tensorflow import keras
 from matplotlib import pyplot as plt
 import os
 import numpy as np
-from generators import gen_pred
 
-fullres_model = keras.models.load_model('fullres_model.h5')
+def gen_pred(test_dir, img, model):
+    rgb_path = os.path.join(test_dir,img)
+    img = cv2.imread(rgb_path)
+    img = tf.expand_dims(img, axis=0)
+    pred = model.predict(img)
+    pred = np.squeeze(pred, axis=0)
+    return cv2.imread(rgb_path), pred
+
+fullres_model = keras.models.load_model('fullres_model & weights/fullres_model.h5')
 
 rows = 1
 columns = 2
 for i in range(len(test_imgs)):
-    img, pred = gen_pred(TEST_DIR, test_imgs[i], fullres_model)
+    img, pred = generators.gen_pred(TEST_DIR, test_imgs[i], fullres_model)
     fig = plt.figure(figsize=(10, 7))
     fig.add_subplot(rows, columns, 1)
     plt.imshow(img)

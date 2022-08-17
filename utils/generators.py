4ef991a12f3335f4b290a441e1b7b64ad638c738
Andrey Radyhin <84941386+Andrii-Radyhin@users.noreply.github.com>
2022-08-17 05:34:18 +0300

    Update generators.py

--- utils/generators.py
+++ utils/generators.py
@@ -4,11 +4,27 @@ import pandas as pd
 import os
 from skimage.io import imread
 from skimage.morphology import label
-from utils import masks_as_image
+from utils import utils
 import tensorflow as tf
 import cv2
+from keras.preprocessing.image import ImageDataGenerator
 
-def gen_pred(test_dir, img, model)
+dg_args = dict(featurewise_center = False, 
+                  samplewise_center = False,
+                  rotation_range = 45, 
+                  width_shift_range = 0.1, 
+                  height_shift_range = 0.1, 
+                  shear_range = 0.01,
+                  zoom_range = [0.9, 1.25],  
+                  horizontal_flip = True, 
+                  vertical_flip = True,
+                  fill_mode = 'reflect',
+                   data_format = 'channels_last')
+
+image_gen = ImageDataGenerator(**dg_args)
+label_gen = ImageDataGenerator(**dg_args)
+
+def gen_pred(test_dir, img, model):
     rgb_path = os.path.join(TEST_DIR,img)
     img = cv2.imread(rgb_path)
     img = tf.expand_dims(img, axis=0)
@@ -16,19 +32,19 @@ def gen_pred(test_dir, img, model)
     pred = np.squeeze(pred, axis=0)
     return cv2.imread(rgb_path), pred
 
-def make_image_gen(in_df, batch_size = BATCH_SIZE):
+def make_image_gen(in_df, batch_size = config.BATCH_SIZE):
     all_batches = list(in_df.groupby('ImageId'))
     out_rgb = []
     out_mask = []
     while True:
         np.random.shuffle(all_batches)
         for c_img_id, c_masks in all_batches:
-            rgb_path = os.path.join(TRAIN_DIR, c_img_id)
+            rgb_path = os.path.join(config.TRAIN_DIR, c_img_id)
             c_img = imread(rgb_path)
-            c_mask = np.expand_dims(masks_as_image(c_masks['EncodedPixels'].values), -1)
-            if IMG_SCALING is not None:
-                c_img = c_img[::IMG_SCALING[0], ::IMG_SCALING[1]]
-                c_mask = c_mask[::IMG_SCALING[0], ::IMG_SCALING[1]]
+            c_mask = np.expand_dims(utils.masks_as_image(c_masks['EncodedPixels'].values), -1)
+            if config.IMG_SCALING is not None:
+                c_img = c_img[::config.IMG_SCALING[0], ::config.IMG_SCALING[1]]
+                c_mask = c_mask[::config.IMG_SCALING[0], ::config.IMG_SCALING[1]]
             out_rgb += [c_img]
             out_mask += [c_mask]
             if len(out_rgb)>=batch_size:

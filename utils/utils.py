e8bdb3f81385e24bc0a8be45deea23f717296906
Andrey Radyhin <84941386+Andrii-Radyhin@users.noreply.github.com>
2022-08-26 20:41:28 +0300

    Create utils.py

---
+++ utils/utils.py
@@ -0,0 +1,30 @@
+import config
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from skimage.io import imread
+from skimage.morphology import binary_opening, disk, label
+
+
+def rle_decode(mask_rle, shape=(768, 768)):
+    '''
+    mask_rle: run-length as string formated (start length)
+    shape: (height,width) of array to return 
+    Returns numpy array, 1 - mask, 0 - background
+    '''
+    s = mask_rle.split()
+    starts, lengths = [np.asarray(x, dtype=int) for x in (s[0:][::2], s[1:][::2])]
+    starts -= 1
+    ends = starts + lengths
+    img = np.zeros(shape[0]*shape[1], dtype=np.uint8)
+    for lo, hi in zip(starts, ends):
+        img[lo:hi] = 1
+    return img.reshape(shape).T  # Needed to align to RLE direction
+
+def masks_as_image(in_mask_list):
+    # Take the individual ship masks and create a single mask array for all ships
+    all_masks = np.zeros((768, 768), dtype = np.uint8)
+    for mask in in_mask_list:
+        if isinstance(mask, str):
+            all_masks |= rle_decode(mask)
+    return all_masks

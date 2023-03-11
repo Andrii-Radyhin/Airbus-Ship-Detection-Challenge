d58ae0cd45eb37ee22b8eb38e8d849aea5b14c08
Andrey Radyhin <84941386+Andrii-Radyhin@users.noreply.github.com>
2023-03-11 14:37:05 +0200

    Update train.py

--- train.py
+++ train.py
@@ -96,61 +96,59 @@ else:
     upsample=upsample_simple
     
 def unet(pretrained_weights = None, input_size = (256, 256, 3), NET_SCALING = NET_SCALING):
-    input_img = layers.Input(input_size, name = 'RGB_Input')
-    pp_in_layer = input_img
+    inputs = layers.Input(input_size)
 
-    if NET_SCALING is not None:
-        pp_in_layer = layers.AvgPool2D(NET_SCALING)(pp_in_layer)
-    
-    pp_in_layer = layers.GaussianNoise(GAUSSIAN_NOISE)(pp_in_layer)
-    pp_in_layer = layers.BatchNormalization()(pp_in_layer)
+    c1 = layers.Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(inputs)
+    c1 = layers.Dropout(0.1)(c1)
+    c1 = layers.Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c1)
+    p1 = layers.MaxPooling2D((2, 2))(c1)
 
-    c1 = layers.Conv2D(8, (3, 3), activation='relu', padding='same') (pp_in_layer)
-    c1 = layers.Conv2D(8, (3, 3), activation='relu', padding='same') (c1)
-    p1 = layers.MaxPooling2D((2, 2)) (c1)
+    c2 = layers.Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p1)
+    c2 = layers.Dropout(0.1)(c2)
+    c2 = layers.Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c2)
+    p2 = layers.MaxPooling2D((2, 2))(c2)
 
-    c2 = layers.Conv2D(16, (3, 3), activation='relu', padding='same') (p1)
-    c2 = layers.Conv2D(16, (3, 3), activation='relu', padding='same') (c2)
-    p2 = layers.MaxPooling2D((2, 2)) (c2)
+    c3 = layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p2)
+    c3 = layers.Dropout(0.2)(c3)
+    c3 = layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c3)
+    p3 = layers.MaxPooling2D((2, 2))(c3)
 
-    c3 = layers.Conv2D(32, (3, 3), activation='relu', padding='same') (p2)
-    c3 = layers.Conv2D(32, (3, 3), activation='relu', padding='same') (c3)
-    p3 = layers.MaxPooling2D((2, 2)) (c3)
-    
-    c4 = layers.Conv2D(64, (3, 3), activation='relu', padding='same') (p3)
-    c4 = layers.Conv2D(64, (3, 3), activation='relu', padding='same') (c4)
-    p4 = layers.MaxPooling2D(pool_size=(2, 2)) (c4)
+    c4 = layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p3)
+    c4 = layers.Dropout(0.2)(c4)
+    c4 = layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c4)
+    p4 = layers.MaxPooling2D(pool_size=(2, 2))(c4)
 
+    c5 = layers.Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p4)
+    c5 = layers.Dropout(0.3)(c5)
+    c5 = layers.Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c5)
 
-    c5 = layers.Conv2D(128, (3, 3), activation='relu', padding='same') (p4)
-    c5 = layers.Conv2D(128, (3, 3), activation='relu', padding='same') (c5)
-
-    u6 = upsample(64, (2, 2), strides=(2, 2), padding='same') (c5)
+    u6 = layers.Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(c5)
     u6 = layers.concatenate([u6, c4])
-    c6 = layers.Conv2D(64, (3, 3), activation='relu', padding='same') (u6)
-    c6 = layers.Conv2D(64, (3, 3), activation='relu', padding='same') (c6)
+    c6 = layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u6)
+    c6 = layers.Dropout(0.2)(c6)
+    c6 = layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c6)
 
-    u7 = upsample(32, (2, 2), strides=(2, 2), padding='same') (c6)
+    u7 = layers.Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(c6)
     u7 = layers.concatenate([u7, c3])
-    c7 = layers.Conv2D(32, (3, 3), activation='relu', padding='same') (u7)
-    c7 = layers.Conv2D(32, (3, 3), activation='relu', padding='same') (c7)
+    c7 = layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u7)
+    c7 = layers.Dropout(0.2)(c7)
+    c7 = layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c7)
 
-    u8 = upsample(16, (2, 2), strides=(2, 2), padding='same') (c7)
+    u8 = layers.Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(c7)
     u8 = layers.concatenate([u8, c2])
-    c8 = layers.Conv2D(16, (3, 3), activation='relu', padding='same') (u8)
-    c8 = layers.Conv2D(16, (3, 3), activation='relu', padding='same') (c8)
+    c8 = layers.Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u8)
+    c8 = layers.Dropout(0.1)(c8)
+    c8 = layers.Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c8)
 
-    u9 = upsample(8, (2, 2), strides=(2, 2), padding='same') (c8)
+    u9 = layers.Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same')(c8)
     u9 = layers.concatenate([u9, c1], axis=3)
-    c9 = layers.Conv2D(8, (3, 3), activation='relu', padding='same') (u9)
-    c9 = layers.Conv2D(8, (3, 3), activation='relu', padding='same') (c9)
+    c9 = layers.Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u9)
+    c9 = layers.Dropout(0.1)(c9)
+    c9 = layers.Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c9)
 
-    d = layers.Conv2D(1, (1, 1), activation='sigmoid') (c9)
+    d = layers.Conv2D(1, (1, 1), activation='sigmoid')(c9)
     
-    if NET_SCALING is not None:
-        d = layers.UpSampling2D(NET_SCALING)(d)
-    
-    seg_model = models.Model(inputs=[input_img], outputs=[d])
+    seg_model = models.Model(inputs=[inputs], outputs=[d])
     #seg_model.summary()
     
     if(pretrained_weights):
@@ -164,13 +162,13 @@ seg_model = unet()
 weight_path="fullres_model & weights/{}_weights.best.hdf5".format('seg_model')
 
 checkpoint = ModelCheckpoint(weight_path, monitor='val_dice_coef', verbose=1, 
-                             save_best_only=True, mode='min', save_weights_only = True)
+                             save_best_only=True, mode='max', save_weights_only = True)
 
 reduceLROnPlat = ReduceLROnPlateau(monitor='val_dice_coef', factor=0.5, 
                                    patience=3, 
-                                   verbose=1, mode='min', epsilon=0.0001, cooldown=2, min_lr=1e-6)
+                                   verbose=1, mode='max', epsilon=0.0001, cooldown=2, min_lr=1e-6)
 early = EarlyStopping(monitor="val_dice_coef", 
-                      mode="min", 
+                      mode="max", 
                       patience=15)
 callbacks_list = [checkpoint, early, reduceLROnPlat]
 
